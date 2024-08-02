f0fa80987b5c95dd492fc5dd2ec6747c9af6372e
hmxs <hmxsqaq@163.com>
2024-08-03 02:23:50 +0800

    feat(sort): add bubble_sort

--- src/sorting/bubble_sort.h
+++ src/sorting/bubble_sort.h
@@ -1,6 +1,22 @@
 #ifndef BUBBLE_SORT_H
 #define BUBBLE_SORT_H
 
+#include "sort.h"
+#include "utility/timer.h"
 
+class BubbleSort {
+public:
+    template <typename T>
+    static void Sort(T* array, const size_t size) {
+        Timer timer("BubbleSort");
+        for (size_t i = 0; i < size - 1; i++) {
+            for (size_t j = 0; j < size - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    Swap(array[j], array[j + 1]);
+                }
+            }
+        }
+    }
+};
 
 #endif //BUBBLE_SORT_H
